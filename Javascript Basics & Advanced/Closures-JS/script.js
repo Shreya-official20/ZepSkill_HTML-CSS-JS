7fac7f6f08ec9eea3a6c4a4a45fd807c7d07b5ed
ShreyaOfficial <shreyasingh202001@gmail.com>
2023-04-18 07:21:14 +0530

    modified

--- Javascript Basics & Advanced/Closures-JS/script.js
+++ Javascript Basics & Advanced/Closures-JS/script.js
@@ -9,15 +9,15 @@
 // }
 // x();
 
-function x(){
-    var a = 20;
-    function y(){
-        console.log(a);
-    }
-    return y;
-}
-var z = x();
-console.log(z);     // this will return o/p: f y(){console.log(a);}
+// function x(){
+//     var a = 20;
+//     function y(){
+//         console.log(a);
+//     }
+//     return y;
+// }
+// var z = x();
+// console.log(z);     // this will return o/p: f y(){console.log(a);}
 
 //... if we write 1000lines of codes here still functions remember, where they actually present 
-z();                // this will return >> o/p: 20
+// z();                // this will return >> o/p: 20
